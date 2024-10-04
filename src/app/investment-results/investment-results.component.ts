bc3ac00f6cf012d579262bcab475647a5cf65f2a
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2024-10-04 19:27:45 +0200

    app is done

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { UserInput } from '../user-input/user-input.model';
 import { Calculations } from './investment.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
